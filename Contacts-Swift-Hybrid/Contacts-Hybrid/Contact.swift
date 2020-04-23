ed9f97d4b441a90d75ee383edd77c9d13258ff5f
dillonp23 <dillonp.lambda@gmail.com>
2020-04-23 15:54:46 -0700

    Added a Swift Contact class

---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/Contact.swift
@@ -0,0 +1,13 @@
+//
+//  Contact.swift
+//  Contacts-Hybrid
+//
+//  Created by Dillon P on 4/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+import Foundation
+
+class Contact {
+    
+}
